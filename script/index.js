d19c11cfa886de74783569c321f379253f394e04
Ujjawal-kumar19 <kumar9ujjawal2014@gmail.com>
2021-11-30 21:44:37 +0530

    hero and nav bar

---
+++ script/index.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 25rem  (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "30rem";
+  document.getElementById("overlay").style.width = "100vw";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+  document.getElementById("overlay").style.width = "0";
+}
