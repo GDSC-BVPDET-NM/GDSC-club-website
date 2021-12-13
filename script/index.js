cf4a44c0298e1f204ac679bbc3f3530d7b7f3c48
toHarsh <harshkothari17@gmail.com>
2021-12-13 06:15:27 +0530

    changes made

--- script/index.js
+++ script/index.js
@@ -9,3 +9,24 @@ function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
   document.getElementById("overlay").style.width = "0";
 }
+
+var request = new XMLHttpRequest();
+        request.open('GET','https://api.github.com/users/GDSC-BVP-DET-Navi-Mumbai/repos' , 
+        true)
+        request.onload = function() {
+            var data = JSON.parse(this.response);
+            console.log(data);
+            var statusHTML = '';
+            $.each(data, function(i, status){
+                statusHTML += '<a class="proj-link" href=""> \
+                <div class="project-card"> \
+                    <h4>' + status.name +  '</h4> \
+                    <div class="state"> \
+                        \
+                    </div> \
+                </div> \
+           </a> ';
+            });
+            $('.repositories').html(statusHTML);
+        }
+        request.send();
